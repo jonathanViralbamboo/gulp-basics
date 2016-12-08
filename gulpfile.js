840ad793683a7afe00792ab36acfc4d271248df0
jonathanViralbamboo <jonathan@viralbamboo.com>
2016-12-08 19:41:00 +0000

    Putting Multiple Tasks Together
    Putting multiple tasks together can allow you to carry out the tasks
    using a single command in the terminal

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var maps = require('gulp-sourcemaps');
 gulp.task("concatScripts", function() {
   // Gets the readable array of files to be concatenated.
   // Note: the order of the files in the array does matter, so order them accordingly.
-  gulp.src([
+  return gulp.src([
     'js.jquery.js',
     'js/sticky/jquery.sticky.js',
     'js/main'])
@@ -22,8 +22,9 @@ gulp.task("concatScripts", function() {
 });
 
 // 'gulp minifyScripts'
-gulp.task("minifyScripts", function() {
-  gulp.src("js/app.js")         // Gets the file to be minified
+// concatScripts added as a dependency for minifyScrips
+gulp.task("minifyScripts", ["concatScripts"], function() {
+  return gulp.src("js/app.js")         // Gets the file to be minified
     .pipe(uglify())             // Calls the uglify method
     .pipe(rename('app.min.js')) // Rename the minified file to app.min.js
     .pipe(gulp.dest('js'));     // Sets the destination folder
@@ -31,13 +32,16 @@ gulp.task("minifyScripts", function() {
 
 // 'gulp compileSass'
 gulp.task('compileSass', function() {
-  gulp.src("scss/application.scss") // Gets the file to be compiled
+   return gulp.src("scss/application.scss") // Gets the file to be compiled
     .pipe(maps.init())              // Runs the maps.init method for sass
     .pipe(sass())                   // Compile source into css
     .pipe(maps.write('./'))         // Current directory, relevant to the gulp.dest() directory (css in this case)
     .pipe(gulp.dest('css'));        // Save compiled file into the css folder
 });
 
-gulp.task("default", ["hello"], function() {
-  console.log("Default Task!");
-});
+//'gulp build'
+// Getting an array of dependencies to run
+gulp.task("build", ['minifyScripts', 'compileSass']);
+
+// 'gulp'
+gulp.task("default", ["build"]);
