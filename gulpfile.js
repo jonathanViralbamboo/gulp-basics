967a0866305d6af1510353d8d35824430248ea27
jonathanViralbamboo <jonathan@viralbamboo.com>
2016-12-08 18:55:03 +0000

    Minifying JavaScript Files
    Creating a gulp task to minify the projects app.js file

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
 
 // 'gulp concatScripts'
 gulp.task("concatScripts", function() {
@@ -13,6 +15,13 @@ gulp.task("concatScripts", function() {
     'js/main'])
   .pipe(concat("app.js")) // Concatenates the files in the array to a file called app.js
   .pipe(gulp.dest("js")); // Name of the folder the new app.js file will be saved to.
+});
+
+gulp.task("minifyScripts", function() {
+  gulp.src("js/app.js")         // Gets the file to be minified
+    .pipe(uglify())             // Calls the uglify method
+    .pipe(rename('app.min.js')) // Rename the minified file to app.min.js
+    .pipe(gulp.dest('js'));     // Sets the destination folder
 });
 
 gulp.task("default", ["hello"], function() {
