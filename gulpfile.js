87c02739a21e083aeed5b621c9c86e7be51fa4c1
jonathanViralbamboo <jonathan@viralbamboo.com>
2016-12-08 18:39:36 +0000

    Using Third Party Gulp Libraries
    Using a gulp plugin to concatenate the projects JavaScript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,18 @@
 'use strict';
 
 var gulp = require('gulp');
+var concat = require('gulp-concat');
 
-gulp.task("hello", function() {
-  console.log("Hello!");
+// 'gulp concatScripts'
+gulp.task("concatScripts", function() {
+  // Gets the readable array of files to be concatenated.
+  // Note: the order of the files in the array does matter, so order them accordingly.
+  gulp.src([
+    'js.jquery.js',
+    'js/sticky/jquery.sticky.js',
+    'js/main'])
+  .pipe(concat("app.js")) // Concatenates the files in the array to a file called app.js
+  .pipe(gulp.dest("js")); // Name of the folder the new app.js file will be saved to.
 });
 
 gulp.task("default", ["hello"], function() {
