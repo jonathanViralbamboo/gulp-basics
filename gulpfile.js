7352ac59725bddf2242d6dc8b3b2f03d1f674606
jonathanViralbamboo <jonathan@viralbamboo.com>
2016-12-12 18:21:45 +0000

    The Build and Development Pipeline
    Task runners can do just about anything. One of the most common uses is
    for 1) building applications for deployment, and 2) running our
    application in development. In this video you’ll get an overview of the
    difference, and how you might think about setting up the two task
    pipelines in your project

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var sass = require('gulp-sass');
 var maps = require('gulp-sourcemaps');
+var del = require('del');
 
 // 'gulp concatScripts'
 gulp.task("concatScripts", function() {
@@ -15,16 +16,16 @@ gulp.task("concatScripts", function() {
     'js.jquery.js',
     'js/sticky/jquery.sticky.js',
     'js/main'])
-  .pipe(maps.init())      // Runs the maps.init method for concat
-  .pipe(concat("app.js")) // Concatenates the files in the array to a file called app.js
-  .pipe(maps.write('../maps')) // Current directory, relevant to the gulp.dest() directory (js in this case)
-  .pipe(gulp.dest("js")); // Name of the folder the new app.js file will be saved to.
+  .pipe(maps.init())            // Runs the maps.init method for concat
+  .pipe(concat("app.js"))       // Concatenates the files in the array to a file called app.js
+  .pipe(maps.write('../maps'))  // Current directory, relevant to the gulp.dest() directory (js in this case)
+  .pipe(gulp.dest("js"));       // Name of the folder the new app.js file will be saved to.
 });
 
 // 'gulp minifyScripts'
 // concatScripts added as a dependency for minifyScrips
 gulp.task("minifyScripts", ["concatScripts"], function() {
-  return gulp.src("js/app.js")         // Gets the file to be minified
+  return gulp.src("js/app.js")  // Gets the file to be minified
     .pipe(uglify())             // Calls the uglify method
     .pipe(rename('app.min.js')) // Rename the minified file to app.min.js
     .pipe(gulp.dest('js'));     // Sets the destination folder
@@ -32,20 +33,31 @@ gulp.task("minifyScripts", ["concatScripts"], function() {
 
 // 'gulp compileSass'
 gulp.task('compileSass', function() {
-  return gulp.src("scss/application.scss") // Gets the file to be compiled
-    .pipe(maps.init())              // Runs the maps.init method for sass
-    .pipe(sass())                   // Compile source into css
-    .pipe(maps.write('./'))         // Current directory, relevant to the gulp.dest() directory (css in this case)
-    .pipe(gulp.dest('css'));        // Save compiled file into the css folder
+  return gulp.src("scss/application.scss")  // Gets the file to be compiled
+    .pipe(maps.init())                      // Runs the maps.init method for sass
+    .pipe(sass())                           // Compile source into css
+    .pipe(maps.write('./'))                 // Current directory, relevant to the gulp.dest() directory (css in this case)
+    .pipe(gulp.dest('css'));                // Save compiled file into the css folder
 });
 
 gulp.task('watchSass', function() {
   gulp.watch('scss/**/*.scss', ['compileSass']); // Look in the scss folder / all of its sub directories / any file with the .scss extension
 });
 
+// 'gulp clean'
+gulp.task('clean', function() {
+  del(['dist', 'css/application.css*', 'js/app.*.js*']);  // Deletes the dist folder, as 'build' won't override old files by different names
+});
+
 //'gulp build'
 // Getting an array of dependencies to run
-gulp.task("build", ['minifyScripts', 'compileSass']);
+gulp.task("build", ['minifyScripts', 'compileSass'], function() {
+  return gulp.src(["css/application.css", "js/app.min.js", "index.html",
+      "img/**", "fonts/**"], { base: './'}) // Base tells gulp to keep the directory structure for everything that's provided in the source
+    .pipe(gulp.dest('dist'));               // Save project that's ready for distribution in the 'dist' file
+});
 
 // 'gulp'
-gulp.task("default", ["build"]);
+gulp.task("default", ["clean"], function() {
+  gulp.start('build'); // The build task will run after the clean task has finished
+});
